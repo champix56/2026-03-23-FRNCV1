8e62e56e81b4685deaa48b4c378c8b2be1d90ce6
champix56 <desorbaix@free.fr>
2026-03-24 20:35:02 +0100

    test des composants cart et editor

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
 import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 import React, { useEffect, useState } from "react";
-import { IProduct } from "./src/interfaces/IProducts";
+import { emptyProduct, IProduct } from "./src/interfaces/IProducts";
 import ProductsListViewer from "./src/components/ui/ProductsListViewer/ProductsListViewer.connected";
 import Banner from "./src/components/ui/Banner/Banner";
 import {store} from './src/store/store'
 import { Provider } from "react-redux";
 import ProductsSearcher from "./src/components/functionnals/ProductsSearcher/ProductsSearcher.connected";
+import ProductEditor from "./src/components/ui/ProductEditor/ProductEditor";
+import Cart from "./src/components/ui/Cart/Cart";
 export default function App() {
+const [selected, setselected] = useState(emptyProduct)
   return (
     <Provider store={store}>
       <View style={{ flex: 1, }}>
-        <Banner/>
+        {/* <Banner/>
         <ProductsSearcher/>
         <ScrollView>
           <ProductsListViewer/>
         </ScrollView>
+        */}
+        {store.getState().stock.products.length>0&&<Cart products={store.getState().stock.products.filter((e,i)=>i<=6)}/>}
+        {store.getState().stock.products.length>0&&<ProductEditor product={store.getState().stock.products[0]} onProductSaved={pr=>{setselected(pr)}}/>}
       </View>
     </Provider>
   );
